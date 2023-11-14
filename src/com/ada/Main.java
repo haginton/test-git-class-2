2e9dfcb3b84af49bda03ab789cfc48c99f89fadd
Harrison Martinez <jonathan.martinez@corusconsulting.com>
2023-11-14 18:39:51 -0500

    Add suma method

--- src/com/ada/Main.java
+++ src/com/ada/Main.java
@@ -3,6 +3,14 @@ package com.ada;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    double number1 = 5;
+	    double number2 = 5;
+
+        System.out.println("La suma es: " + suma(number1, number2));
+
+    }
+
+    private static double suma(double number1, double number2) {
+        return number1 + number2;
     }
 }
